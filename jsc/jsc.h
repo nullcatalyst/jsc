f0e802c1830c3dffed0b2279607f223c81821a46
Scott Bennett <scottbennett912@gmail.com>
2016-04-24 21:08:09 -0500

    Initial Commit

---
+++ jsc/jsc.h
@@ -0,0 +1,19 @@
+//
+//  jsc.h
+//  jsc
+//
+//  Created by Scott Bennett on 2016-04-24.
+//  Copyright © 2016 Scott Bennett. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for jsc.
+FOUNDATION_EXPORT double jscVersionNumber;
+
+//! Project version string for jsc.
+FOUNDATION_EXPORT const unsigned char jscVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <jsc/PublicHeader.h>
+
+
